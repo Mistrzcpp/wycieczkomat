bf1f04dd24b8e323ba7cce42550a618c342fe00a
WOK <www.wojtek.wok@gmail.com>
2024-11-18 14:15:05 +0100

    Js validation modify

--- assets/validation.js
+++ assets/validation.js
@@ -1,23 +1,32 @@
 function validateInputLength(element, length){
     let inputValue = $(element).val();
-    if(inputValue.length > length)
+    if(inputValue.length > length){
         $(element).addClass('is-invalid');
-    else
+        $('#saveButton').attr('disabled');
+    }
+    else{
         $(element).removeClass('is-invalid');
-    if(inputValue == '')
+        $('#saveButton').removeAttr('disabled');
+    }
+    if(inputValue == ''){
         $(element).removeClass('is-invalid');
+        $('#saveButton').removeAttr('disabled');
+    }
 }
 $('#telefon').on('input', function(e){
     let phone = $(this).val();
     let regex = /^\d+$/;
     if (phone.length > 9 || !regex.test(phone)) {
         $(this).addClass('is-invalid');
+        $('#saveButton').attr('disabled');
     }
     else {
         $(this).removeClass('is-invalid');
+        $('#saveButton').removeAttr('disabled');
     }
     if(phone == '') {
         $(this).removeClass('is-invalid');
+        $('#saveButton').removeAttr('disabled');
     }
 })
 $('#klasa').on('input', function(){
